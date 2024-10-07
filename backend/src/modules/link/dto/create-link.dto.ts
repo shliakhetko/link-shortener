0e6ad168c35f65a8bf203e07c6f0c351edcaf5c8
daniel-shliakhetko <danielshliakhetko@gmail.com>
2024-10-07 13:31:21 +0300

    Add CreateLinkDto

---
+++ backend/src/modules/link/dto/create-link.dto.ts
@@ -0,0 +1,13 @@
+﻿import {IsOptional, IsString, IsUrl} from "class-validator";
+
+export class CreateLinkDto {
+    @IsOptional()
+    @IsString()
+    title?: string;
+    
+    @IsUrl()
+    link: string;
+    
+    @IsString()
+    shortLink: string;
+}
