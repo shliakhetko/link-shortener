ab564ab8830029eaecdceaeef81b91af773621ef
daniel-shliakhetko <danielshliakhetko@gmail.com>
2024-10-07 15:54:39 +0300

    Add User model

---
+++ backend/src/modules/user/models/user.model.ts
@@ -0,0 +1,8 @@
+﻿import {BaseModel} from "../../../common/models/base.model";
+
+export class User extends BaseModel{
+    username: string;
+    email: string;
+    password: string;
+    links: string[];
+}
