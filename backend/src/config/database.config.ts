02be93a8fd43a6c621626c61deec3fcc3a40f348
daniel-shliakhetko <danielshliakhetko@gmail.com>
2024-10-07 12:40:01 +0300

    Setup mongoDB

---
+++ backend/src/config/database.config.ts
@@ -0,0 +1,11 @@
+﻿import * as process from "node:process";
+
+export const databaseConfig = {
+    uri: process.env.MONGODB_URI || 'mongodb://localhost:27017/your_database_name',
+    options: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+};
---
+++ backend/src/database/database.module.ts
@@ -0,0 +1,19 @@
+﻿import {Module} from "@nestjs/common";
+import {MongooseModule} from "@nestjs/mongoose";
+import {ConfigModule, ConfigService} from "@nestjs/config";
+import {databaseConfig} from "../config/database.config";
+
+@Module({
+    imports: [
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: async () => ({
+                uri: databaseConfig.uri,
+                ...databaseConfig.options,
+            }),
+            inject: [ConfigService],
+        })
+    ],
+    exports: [MongooseModule]
+})
+export class DatabaseModule {}
