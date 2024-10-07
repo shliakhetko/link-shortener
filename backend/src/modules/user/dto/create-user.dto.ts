fbd6f48064b061f5138496a8348f9ddafab2082a
daniel-shliakhetko <danielshliakhetko@gmail.com>
2024-10-07 16:13:40 +0300

    Add CreateUserDto

---
+++ backend/src/modules/user/dto/create-user.dto.ts
@@ -0,0 +1,19 @@
+﻿import {IsEmail, IsNotEmpty, IsString, IsStrongPassword} from "class-validator";
+
+export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty()
+    username: string;
+
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+    
+    @IsString()
+    @IsNotEmpty()
+    password: string;
+    
+    @IsString({each: true})
+    @IsStrongPassword()
+    links: string[];
+}
