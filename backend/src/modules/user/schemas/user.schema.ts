12f202b74696e3fc2bd3c770f92cee5e287154d9
daniel-shliakhetko <danielshliakhetko@gmail.com>
2024-10-07 15:57:49 +0300

    Add User schema

---
+++ backend/src/modules/user/schemas/user.schema.ts
@@ -0,0 +1,18 @@
+﻿import {Prop, Schema, SchemaFactory} from "@nestjs/mongoose";
+import {BaseSchema} from "../../../common/schemas/base.schema";
+
+@Schema()
+export class UserDocument extends BaseSchema {
+    @Prop({required: true, unique: true})
+    username: string;
+    
+    @Prop({required: true, unique: true})
+    email: string;
+    
+    @Prop({required: true})
+    password: string;
+    
+    @Prop({default: []})
+    links: string[];
+}
+export const UserSchema = SchemaFactory.createForClass(UserDocument);
