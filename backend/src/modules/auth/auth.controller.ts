531e79c8d22c5b73e9ec01e99294e6dba1c0df92
daniel-shliakhetko <danielshliakhetko@gmail.com>
2024-10-08 09:49:14 +0300

    Add auth controller

---
+++ backend/src/modules/auth/auth.controller.ts
@@ -0,0 +1,21 @@
+﻿import {Body, Request, Controller, Post, UseGuards} from "@nestjs/common";
+import {AuthService} from "./auth.service";
+import {AuthGuard} from "@nestjs/passport";
+import {LoginDto} from "./dto/login.dto";
+
+@Controller('auth')
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+    
+    @UseGuards(AuthGuard('local'))
+    @Post('login')
+    async login(@Request() req:any, @Body() loginDto: LoginDto) {
+        return this.authService.login(req.user);
+    }
+    
+    @UseGuards(AuthGuard('jwt'))
+    @Post('profile')
+    getProfile(@Request() req:any) {
+        return req.user;
+    }
+}
